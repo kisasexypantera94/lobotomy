1461d5514ebf98e27f399a0b1ceeb72ceb4db836
David Grinberg <green.grinya@gmail.com>
2023-10-27 19:56:00 +0400

    update

--- src/order_book/price_top.rs
+++ src/order_book/price_top.rs
@@ -1,21 +1,5 @@
 use crate::common::utils::round_to_tick_size;
 
-/// Cases:
-///
-/// - Amount > 0 (Upsert):
-///     1. Find the position for insertion/update. If the position is beyond the top (None), then take no action.
-///     2. If we find the exact price, simply return.
-///     3. Shift to the right from the insertion position.
-///     4. Insert the new price.
-///
-/// - Amount == 0 (Delete):
-///     1. Find the position for the price. If the position is beyond the top (None), take no action.
-///        Also, find the position of the worst price in the top.
-///     2. If `top[pos] != price`, return.
-///     3. If the position of the worst price is None, it means the top is empty - return.
-///     4. When we delete the price, there will be a shift to the left,
-///        leaving an empty spot at the position of the worst price. Therefore, we need to ask PriceMap for the next worst price with amount > 0.
-///     5. Insert the next worst price in the empty spot.
 #[derive(Debug)]
 pub struct PriceTop<const N: usize, const REVERSE: bool> {
     top: Vec<f64>,
@@ -30,6 +14,22 @@ impl<const N: usize, const REVERSE: bool> PriceTop<N, REVERSE> {
         }
     }
 
+    /// Cases:
+    ///
+    /// - Amount > 0 (Upsert):
+    ///     1. Find the position for insertion/update. If the position is beyond the top (None), then take no action.
+    ///     2. If we find the exact price, simply return.
+    ///     3. Shift to the right from the insertion position.
+    ///     4. Insert the new price.
+    ///
+    /// - Amount == 0 (Delete):
+    ///     1. Find the position for the price. If the position is beyond the top (None), take no action.
+    ///        Also, find the position of the worst price in the top.
+    ///     2. If `top[pos] != price`, return.
+    ///     3. If the position of the worst price is None, it means the top is empty - return.
+    ///     4. When we delete the price, there will be a shift to the left,
+    ///        leaving an empty spot at the position of the worst price. Therefore, we need to ask PriceMap for the next worst price with amount > 0.
+    ///     5. Insert the next worst price in the empty spot.
     #[inline(always)]
     pub fn update(&mut self, px: f64, amt: f64, get_next_worst_px: impl Fn(f64) -> Option<f64>) {
         let px = round_to_tick_size(px, self.tick_size);
